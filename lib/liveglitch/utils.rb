61ca0a9016f1c6e8a9aec9df96fe3ac1900fb529
James Darling <james@abscond.org>
2014-01-12 19:51:37 +0000

    Stop sending the stream over /test

--- lib/liveglitch/utils.rb
+++ lib/liveglitch/utils.rb
@@ -2,11 +2,11 @@ module LiveGlitch
   class Utils
   	VLC = "/Applications/VLC.app/Contents/MacOS/VLC"
   	def self.serve_file(file_path)
-  		`#{VLC} #{file_path} --sout "#standard{mux=ts,access=http,dst=localhost:9999/test}"`
+  		`#{VLC} #{file_path} --sout "#standard{mux=ts,access=http,dst=localhost:9999}"`
   	end
 
     def self.play_stream()
-      `#{VLC} --http-proxy="http://localhost:8008" http://localhost:9999/test`
+      `#{VLC} --http-proxy="http://localhost:8008" http://localhost:9999`
     end
   end
 end
